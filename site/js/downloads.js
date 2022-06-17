59c7493f6d418904309fa4fa5d10c1cab18ad180
Wenchen Fan <wenchen@databricks.com>
2022-06-17 14:10:00 +0800

    Update downloads.js

--- site/js/downloads.js
+++ site/js/downloads.js
@@ -17,6 +17,9 @@ var hadoop2p7 = {pretty: "Pre-built for Apache Hadoop 2.7", tag: "hadoop2.7"};
 var hadoop3p2 = {pretty: "Pre-built for Apache Hadoop 3.2 and later", tag: "hadoop3.2"};
 var hadoop3p3 = {pretty: "Pre-built for Apache Hadoop 3.3 and later", tag: "hadoop3.2"};
 var hadoop3p3scala213 = {pretty: "Pre-built for Apache Hadoop 3.3 and later (Scala 2.13)", tag: "hadoop3.2-scala2.13"};
+var hadoop2p = {pretty: "Pre-built for Apache Hadoop 2.7", tag: "hadoop2"};
+var hadoop3p = {pretty: "Pre-built for Apache Hadoop 3.3 and later", tag: "hadoop3"};
+var hadoop3pscala213 = {pretty: "Pre-built for Apache Hadoop 3.3 and later (Scala 2.13)", tag: "hadoop3-scala2.13"};
 var scala2p12_hadoopFree = {pretty: "Pre-built with Scala 2.12 and user-provided Apache Hadoop", tag: "without-hadoop-scala-2.12"};
 
 // 3.0.0+
@@ -25,8 +28,10 @@ var packagesV10 = [hadoop2p7, hadoop3p2, hadoopFree, sources];
 var packagesV11 = [hadoop3p2, hadoop2p7, hadoopFree, sources];
 // 3.2.0+
 var packagesV12 = [hadoop3p3, hadoop3p3scala213, hadoop2p7, hadoopFree, sources];
+// 3.3.0+
+var packagesV13 = [hadoop3p, hadoop3pscala213, hadoop2p, hadoopFree, sources];
 
-addRelease("3.3.0", new Date("06/16/2022"), packagesV12, true);
+addRelease("3.3.0", new Date("06/16/2022"), packagesV13, true);
 addRelease("3.2.1", new Date("01/26/2022"), packagesV12, true);
 addRelease("3.1.3", new Date("02/18/2022"), packagesV11, true);
 addRelease("3.0.3", new Date("06/23/2021"), packagesV10, true);
