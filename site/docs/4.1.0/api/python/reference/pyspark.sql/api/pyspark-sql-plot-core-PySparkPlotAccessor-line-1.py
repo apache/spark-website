6bb3f0e4e4bd7e6716a7b4bcada24445d96599c0
dongjoon-hyun <dongjoon@apache.org>
2025-12-16 12:33:47 +0000

    Add docs for Apache Spark 4.1.0

---
+++ site/docs/4.1.0/api/python/reference/pyspark.sql/api/pyspark-sql-plot-core-PySparkPlotAccessor-line-1.py
@@ -0,0 +1,6 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.getOrCreate()
+data = [("A", 10, 1.5), ("B", 30, 2.5), ("C", 20, 3.5)]
+columns = ["category", "int_val", "float_val"]
+df = spark.createDataFrame(data, columns)
+df.plot.line(x="category", y="int_val")  # doctest: +SKIP
