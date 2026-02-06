4058a172ee9ec48a561b0d8c20305659aec03c94
Dongjoon Hyun <dongjoon@apache.org>
2026-02-05 16:16:02 -0800

    Redirect 4.1.0-preview* docs to ASF archive service

--- site/docs/4.1.0-preview3/api/python/reference/pyspark.sql/api/pyspark-sql-plot-core-PySparkPlotAccessor-scatter-1.py
+++
@@ -1,6 +0,0 @@
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.getOrCreate()
-data = [(5.1, 3.5, 0), (4.9, 3.0, 0), (7.0, 3.2, 1), (6.4, 3.2, 1), (5.9, 3.0, 2)]
-columns = ['length', 'width', 'species']
-df = spark.createDataFrame(data, columns)
-df.plot.scatter(x='length', y='width')  # doctest: +SKIP
