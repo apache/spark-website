8b23fb9af2e99d0b11c386f1024f51729558d813
dongjoon-hyun <dongjoon@apache.org>
2026-06-11 07:15:21 +0000

    Add release docs for Apache Spark 4.0.3

--- js/downloads.js
+++ js/downloads.js
@@ -24,7 +24,7 @@ var packagesV14 = [hadoop3p, hadoop3pscala213, hadoopFree, sources];
 var packagesV15 = [hadoop34p, hadoop34pSparkConnect, hadoopFree, sources];
 
 addRelease("4.1.2", new Date("05/21/2026"), packagesV15, true);
-addRelease("4.0.2", new Date("02/05/2026"), packagesV15, true);
+addRelease("4.0.3", new Date("06/11/2026"), packagesV15, true);
 addRelease("3.5.8", new Date("01/15/2026"), packagesV14, true);
 
 function append(el, contents) {
